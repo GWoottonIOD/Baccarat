de22767acb7e5720c781a9078082d46171721e30
Gareth Wootton <gareth.wootton@institutedata.com>
2024-04-17 09:47:30 +1200

    Housekeeping - removed unused components and states within HandContext

--- Front-End/src/context/HandContext.jsx
+++ Front-End/src/context/HandContext.jsx
@@ -8,22 +8,11 @@ export const HandContext = React.createContext();
 // HandContext function to provide user data to components
 export const HandHolder = (props) => {
 
-    const [firstCard, setFirstCard] = useState(null)
-    const [secondCard, setSecondCard] = useState(null)
-    const [thirdCard, setThirdCard] = useState(null)
-    const [fourthCard, setFourthCard] = useState(null)
-    const [fifthCard, setFifthCard] = useState(null)
-    const [sixthCard, setSixthCard] = useState(null)
     const [hand, setHand] = useState([]);
 
     return (
         // Providing the current user and handleUser function to the context object
-        <HandContext.Provider value={{
-            firstCard, setFirstCard, secondCard, setSecondCard,
-            thirdCard, setThirdCard, fourthCard, setFourthCard,
-            fifthCard, setFifthCard, sixthCard, setSixthCard,
-            hand, setHand
-            }}>
+        <HandContext.Provider value={{hand, setHand}}>
             {props.children}
         </HandContext.Provider>
     );
